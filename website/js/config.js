f21a3f86f1ce430bfc4d7e313efe266fa8b762dc
Kejia Shi <kejia.shi@gmail.com>
2018-09-11 09:03:23 -0500

    first commit

---
+++ website/js/config.js
@@ -0,0 +1,7 @@
+var configConstants = {
+    auth0: {
+        domain: 'acg-video-lab.auth0.com',
+        clientId: 'PC3vfGDT0Z-T_P8HIuMCtrHRQA4pyHan'
+    },
+    apiBaseUrl: 'https://wl94tppdff.execute-api.us-east-1.amazonaws.com/dev'
+};
